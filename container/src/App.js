084f99edcef5a84d22c8f16c1f3e448f19face76
Carlos <sonne@iMac-de-Carlos.local>
2021-09-02 22:30:18 -0500

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => {
+  return (
+    <>
+      <MarketingApp />
+    </>
+  );
+};
+export default App;
